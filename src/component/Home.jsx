d93ee1bf065c745f8f9819ad23194777a4350b49
MelihKrts <melih.karatas1281@gmail.com>
2024-10-15 22:17:49 +0300

    responsive design compliance arrangement

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -16,11 +16,11 @@ export default function Home() {
             <section className="w-full pt-16">
                 <div className='container flex flex-col justify-center items-center'>
 
-                    <h4 className='py-4 text-4xl font-semibold'>Congrates Card Generator</h4>
+                    <h4 className='text-center sm:text-center sm:text-xl md:text-2xl lg:text-3xl md:text-center lg:text-center py-4 text-4xl font-semibold'>Congrates Card Generator</h4>
 
                     <p className='text-justify px-4 text-xl'>Allows you to create greeting cards for special occasions, loved ones and customizable</p>
 
-                    <Link to="/generator" className='w-1/4 sm:w-1/4 md:w-1/3 lg:w-1/2 flex items-center justify-center'>
+                    <Link to="/generator" className='w-2/3 sm:w-1/3 md:w-1/3 lg:w-1/2 flex items-center justify-center'>
                         <button className=' px-4 py-4 border-none bg-teal-400 rounded-md my-4'>Create Card</button>
                     </Link>
 
