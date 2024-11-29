934d72962095b85b15bd6d584ec1cf62e515bd47
MelihKrts <melih.karatas1281@gmail.com>
2024-11-29 11:51:07 +0300

    edited files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
 const CACHE_NAME = 'card-generator-cache';
 const urlsToCache = ['/', '/index.html', '/icon-192x192.png', '/icon-512x512.png'];
@@ -24,7 +23,5 @@ self.addEventListener('fetch', (event) => {
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 )
